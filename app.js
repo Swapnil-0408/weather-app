58409968051dcf962883dd4e78fd1b035b384091
Swapnil-0408 <swapnil042004@gmail.com>
2026-09-10 00:38:37 +0530

    Add 5-day weather forecast and improve weather UI

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const iconBox = document.querySelector(".iconbox");
 const tempBox = document.querySelector(".tempbox");
 const locationBtn = document.querySelector("#locationBtn");
 console.log(locationBtn);
+const forecastcards = document.querySelector(".forecastcards");
 const extraDetails = document.querySelector(".extradetails");
 const startBtn=document.querySelector(".start");
 const search=document.querySelector("#inputfield");
@@ -17,7 +18,8 @@ const loader=document.querySelector("#loader");
 const mainBox1=document.querySelector(".mainBox1");
 const mainBox2=document.querySelector(".mainBox2");
 const mainBox3=document.querySelector(".mainBox3");
-const url = "https://api.openweathermap.org/data/2.5/weather?";
+const url =  "https://api.openweathermap.org/data/2.5/weather?";
+const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?";
 const apikey = "0ead3ca1215aad52a6ffdb85d6a96fb7";
 async function getWeatherData(city){
   loader.classList.remove("inactive");
@@ -52,6 +54,7 @@ const weatherImages = {
   Fog: "images/mist.png"
 };
 icon.src = weatherImages[weatherMain] || "images/clear.png";
+getForecast(city);
 iconBox.classList.remove("inactive");
 tempBox.classList.remove("inactive");
 extraDetails.classList.remove("inactive");
@@ -68,6 +71,54 @@ finally{
     searchIcon.addEventListener("click",()=>{
     getWeatherData(search.value);
 });
+async function getForecast(city) {
+    const finalurl=`${forecastUrl}q=${city}&appid=${apikey}`;
+    try{
+      const response=await fetch(finalurl);
+      if (!response.ok) {
+    throw new Error("Forecast API failed");
+}
+      const forecastData=await response.json();
+      console.log(forecastData);
+      forecastcards.innerHTML="";
+      const weatherImages = {
+            Clouds: "images/clouds.png",
+            Clear: "images/clear.png",
+            Rain: "images/rain.png",
+            Drizzle: "images/drizzle.png",
+            Snow: "images/snow.png",
+            Thunderstorm: "images/rain.png",
+            Mist: "images/mist.png",
+            Haze: "images/mist.png",
+            Fog: "images/mist.png"
+        };
+      forecastData.list.forEach((item, index) => {
+
+    if(item.dt_txt.includes("12:00:00")){
+    const weatherMain = item.weather[0].main;
+    const icon = weatherImages[weatherMain] || "images/clear.png";
+    const date = new Date(item.dt_txt);
+
+const day = date.toLocaleDateString("en-US", {
+    weekday: "short"
+});
+    forecastcards.innerHTML += `
+    <div class="card">
+     <h4>${day}</h4>
+     <img src="${icon}">
+     <p>${Math.round(item.main.temp - 273.15)}°C</p>
+</div>
+`;
+    }
+
+});
+    }
+    catch(error)
+    {
+      console.log(error);
+    }
+  }
+
 async function getWeatherByLocation(lat, lon) {
 
     loader.classList.remove("inactive");
@@ -85,6 +136,7 @@ async function getWeatherByLocation(lat, lon) {
         desc.innerHTML = weatherData.weather[0].description;
         temp.innerHTML = Math.round(weatherData.main.temp - 273.15) + "°C";
         cityName.innerHTML = weatherData.name;
+        getForecast(weatherData.name);
         wind.innerHTML = weatherData.wind.speed + " km/h";
         humidity.innerHTML = weatherData.main.humidity + "%";
 
@@ -103,7 +155,6 @@ async function getWeatherByLocation(lat, lon) {
         };
 
         icon.src = weatherImages[weatherMain] || "images/clear.png";
-
         iconBox.classList.remove("inactive");
         tempBox.classList.remove("inactive");
         extraDetails.classList.remove("inactive");
